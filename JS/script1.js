9b0f4e350bd664bf0497c160081d384e57a279b9
123123123jon <komanijon372@gmail.com>
2024-11-26 23:28:45 +0100

    Mbarimi i validimit
    Ne kete pjes ka mbaruar verifikimi per karakteret qe duhe te vendosen ne password

--- JS/script1.js
+++ JS/script1.js
@@ -1,11 +1,11 @@
-var password_input = document.getElementById("password");
+const password_input = document.getElementById("password");
+const feedback = document.getElementById("vlersimi");
+const shtyp = document.getElementById("passwordForm");
+const confirm = document.getElementById("confirm");
 
-
-function validoPassword(password){
+function validoPassword(password, confirm2){
 
     const errors=[];
-
-    var jon;
 
     if(!/[A-Z]/.test(password)){
         errors.push("Fjalkalimi duhet te kete se paku nje shkronje te madhe");
@@ -14,13 +14,26 @@ function validoPassword(password){
         errors.push("Fjalkalimi duhet te perfshije gjithsesi nje simbole");
     }
     if(password.length< 9){
-        errors.push("Fjalkalimi duhet te jete i gjate se puku 9-te karaktere");
+        errors.push("Fjalkalimi duhet te jete i gjate se paku 9-te karaktere");
     }
     if(!/[1234567890]/.test(password)){
         errors.push("Fjalkalimi duhet te permbaje nje numer");
     }
+    if(password!=confirm2) {
+        errors.push("Fjalëkalimi dhe konfirmimi nuk përputhen.");
+    }
 
-    return errors.length >0 ? errors : "Fjalkalimi eshte i sakt";
+    return errors;
 }
 
-console.log(validoPassword(password_input.value));
+shtyp.addEventListener("submit", (e) => {
+    const errors = validoPassword(password_input.value, confirm.value);
+    if(errors.length){
+        e.preventDefault();
+        feedback.innerText = errors.join("\n");
+        feedback.style.color="red";
+    } else{
+        feedback.innerText="Fjalkalimi eshte i vlefshem";
+        feedback.style.color="green";
+    }
+})
