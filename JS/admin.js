65bdf54a2c3aa25e6fd0b3b6e15d349a9815cc6b
123123123jon <komanijon372@gmail.com>
2025-01-03 17:12:37 +0100

    Admin

---
+++ JS/admin.js
@@ -0,0 +1,8 @@
+const home = document.querySelector('.home');
+
+function vazhdo() {
+    const rendi2 = document.querySelector('.rendi2');
+    rendi2.style.display = 'flex';
+}
+
+home.addEventListener('click', vazhdo);
