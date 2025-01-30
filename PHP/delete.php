d8a51ab72eccaab1c6e07f1b5d7681c09d2bb9cf
123123123jon <komanijon372@gmail.com>
2025-01-31 00:03:18 +0100

    nje gabim ne delete.php e lidha me PDO ne vend te msqli

--- PHP/delete.php
+++ PHP/delete.php
@@ -9,13 +9,14 @@ if(isset($_GET['id'])){
 
     $sql = "DELETE FROM user WHERE ID=:id";
 
-    $fshije_personin = $conn->prepare($sql);
+    $fshije_personin = $connect->prepare($sql);
 
     $fshije_personin->bindParam(':id', $id);
 
     $fshije_personin->execute();
 
     header("Location: ../PHP/admin.php");
+    exit();
 }else{
     echo "Ka mundesi qe id nuk eshte derguar me sukses";
     exit();
